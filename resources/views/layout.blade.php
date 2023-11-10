d1279d8596564c5ef55bf7e8e964ac6ffe8d4545
Guillaume Orsal (acotango) <guillaume@orsal.net>
2023-11-10 02:39:03 +0100

    CSS + Ajout d'un menu de navigation

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -2,6 +2,9 @@
     <head>
         <title>Storeloc Test</title>
         <style>
+            body {
+                font-family: 'Roboto', Arial;
+            }
             .form {
                 display: flex;
                 gap: 12px;
@@ -12,10 +15,29 @@
                 gap: 12px;
                 justify-content: stretch;
             }
-
+            div {
+                margin: 10px;
+            }
+            table {
+                border-spacing: 0;
+            }
+            td {
+                border: Solid 1px #f2f2f2;
+                padding: 10px;
+            }
+            .font-bold {
+                font-weight: bold;
+            }
         </style>
     </head>
     <body>
+        <nav>
+            <ul>
+                <li><a href="{{ route('index') }}">{{ __('Accueil') }}</a></li>
+                <li><a href="{{ route('services.index') }}">{{ __('Services') }}</a></li>
+                <li><a href="{{ route('stores.index') }}">{{ __('Stores') }}</a></li>
+            </ul>
+        </nav>
         @yield('content')
     </body>
 </html>
