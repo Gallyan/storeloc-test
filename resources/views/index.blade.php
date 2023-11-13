6d1c5ab1187117549221d835410401ffc3e38388
Guillaume Orsal (acotango) <guillaume@orsal.net>
2023-11-13 10:08:35 +0100

    Ajout de l'affichage des messages d'erreur du formulaire

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,21 +2,61 @@
 @section('content')
     <form method="GET" action="{{ route('results') }}" class="form">
         <div class="bounds">
+
             <input type="search" name="n" placeholder="Latitude nord" />
+            @error('n')
+            <span style="color:red" role="alert">
+                <strong>{{ $message }}</strong>
+            </span>
+            @enderror
+
             <input type="search" name="w" placeholder="Longitude ouest" />
+            @error('w')
+            <span style="color:red" role="alert">
+                <strong>{{ $message }}</strong>
+            </span>
+            @enderror
+
             <input type="search" name="s" placeholder="Latitude sud" />
+            @error('s')
+            <span style="color:red" role="alert">
+                <strong>{{ $message }}</strong>
+            </span>
+            @enderror
+
             <input type="search" name="e" placeholder="Longitude est" />
+            @error('e')
+            <span style="color:red" role="alert">
+                <strong>{{ $message }}</strong>
+            </span>
+            @enderror
+
         </div>
         <div class="filters">
+
+            <label for="services[]">{{ __('Services:') }}</label>
             <select multiple name="services[]">
                 @foreach ($services as $service)
                     <option value="{{ $service->id }}">{{ __($service->name) }}</option>
                 @endforeach
             </select>
+            @error('services')
+            <span style="color:red" role="alert">
+                <strong>{{ $message }}</strong>
+            </span>
+            @enderror
+
+            <label for="operator">{{ __('Operator:') }}</label>
             <select name="operator">
                 <option value="OR">OR</option>
                 <option value="AND">AND</option>
             </select>
+            @error('operator')
+            <span style="color:red" role="alert">
+                <strong>{{ $message }}</strong>
+            </span>
+            @enderror
+
         </div>
         <div class="submit">
             <input type="submit" />
