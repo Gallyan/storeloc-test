b03546d136eeedfa7b06373af487a31a0b7b8ec8
Guillaume Orsal (acotango) <guillaume@orsal.net>
2023-11-14 10:22:54 +0100

    Mise en cache des résultats de requêtes

--- app/Http/Controllers/ServicesController.php
+++ app/Http/Controllers/ServicesController.php
@@ -3,18 +3,31 @@
 namespace App\Http\Controllers;
 
 use App\Models\Service;
-use Illuminate\Http\Request;
 
 class ServicesController extends Controller
 {
     public function index()
     {
-        return view('services.index')->with( 'services', Service::all()) ;
+        return view('services.index')
+            ->with( 'services',
+                cache()->remember(
+                    'services_list',
+                    5, // 5 secondes seulement pour pouvoir vérifier que cela fonctionne
+                    fn() => Service::all()
+                )
+            );
     }
 
-    public function show(Service $service)
+    public function show(int $id)
     {
-        return view('services.show')->with( 'service', $service );
+        return view('services.show')
+        ->with( 'service',
+            cache()->remember(
+                'service_' . $id,
+                5, // 5 secondes seulement pour pouvoir vérifier que cela fonctionne
+                fn() => Service::findOrFail( $id )
+            )
+        );
     }
 
 }
--- app/Http/Controllers/StoresController.php
+++ app/Http/Controllers/StoresController.php
@@ -3,18 +3,31 @@
 namespace App\Http\Controllers;
 
 use App\Models\Store;
-use Illuminate\Http\Request;
 
 class StoresController extends Controller
 {
     public function index()
     {
-        return view('stores.index')->with( 'stores', Store::all()) ;
+        return view('stores.index')
+            ->with( 'stores',
+                cache()->remember(
+                    'stores_list',
+                    5, // 5 secondes seulement pour pouvoir vérifier que cela fonctionne
+                    fn() => Store::all()
+                )
+            );
     }
 
-    public function show(Store $store)
+    public function show(int $id)
     {
-        return view('stores.show')->with( 'store', $store );
+        return view('stores.show')
+            ->with( 'store',
+                cache()->remember(
+                    'store_' . $id,
+                    5, // 5 secondes seulement pour pouvoir vérifier que cela fonctionne
+                    fn() => Store::findOrFail( $id )
+                )
+            );
     }
 
 }
