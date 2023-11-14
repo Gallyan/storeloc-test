f87d75482bbfa8458f7ea7e8ad51f3a032506b1b
Guillaume Orsal (acotango) <guillaume@orsal.net>
2023-11-14 10:24:25 +0100

    La liste des stores affiche la liste des services associés, pour contrôler

--- resources/views/stores/index.blade.php
+++ resources/views/stores/index.blade.php
@@ -1,10 +1,13 @@
 @extends('layout')
 @section('content')
     <div>
-        {{ __('Liste des stores:') }}
+        {{ __('Liste des stores (services):') }}
         <ul>
         @forelse( $stores as $store )
-            <li><a href="{{ route('stores.show',$store->id) }}">{{ __($store->name) }}</a></li>
+            <li>
+                <a href="{{ route('stores.show',$store->id) }}">{{ __($store->name) }}</a>
+                ({{ $store->servicesEnum }})
+            </li>
         @empty
             {{ __('Aucun store') }}
         @endforelse
